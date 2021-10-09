ef8c272809e79ea94223e8dfb542fcb0ee9f5356
soo <soo9262001@naver.com>
2021-10-09 14:30:35 +0900

    Initial Commit

---
+++ News Feed App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  News Feed App
+//
+//  Created by kms on 2021/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
